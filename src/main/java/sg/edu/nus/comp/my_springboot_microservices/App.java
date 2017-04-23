fe3c2b02f7226f06e80bfeeb3f7232ea8fbacac4
fanqi1909 <fanqinus@gmail.com>
2017-04-23 13:31:52 +0800

    first commit

---
+++ src/main/java/sg/edu/nus/comp/my_springboot_microservices/App.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.comp.my_springboot_microservices;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
